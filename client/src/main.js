9c5014fb73dab56e454d502e542a218c5d4c5170
Archi398 <archisabatier@gmail.com>
2021-12-09 12:19:25 +0100

    update 12h19

--- client/src/main.js
+++ client/src/main.js
@@ -1,23 +1,42 @@
+import { Socket } from "dgram";
 import { createConnection } from "net";
+import { createInterface } from "readline";
 
 let currentCommand = '';
 let isAuthenticated = false;
 
-const client = createConnection({ port: 4242 }, () => {
-  console.log("client connected.");
+const rl = createInterface({
+  input: process.stdin,
+  output: process.stdout
 });
 
-client.on("data", (data) => {
-  const message = data.toString();
-  console.log("Message received:", message);
+const client = createConnection({ port: 4242 }, () => {
+  console.log("client connected.");
+  console.log("Please connect with command USER.");
+  
+  rl.question('Command : ', function (cmd) {
+    client.write(cmd)
+  })
 
-  const [status, ...args] = message.trim().split(" ");
-  if (status == 230 && currentCommand === "USER") {
-    isAuthenticated = true;
-  }
+  client.on("data", (data) => {
+    const message = data.toString();
+    console.log("Message received:", message);
+    rl.question('Command : ', function (cmd) {
+      client.write(cmd)
+    }) 
 
-  if (status == 220) {
-    currentCommand = "USER";
-    client.write("USER anonymous");
-  };
+    const [status, ...args] = message.trim().split(" ");
+    if (status == 230 && currentCommand === "USER") {
+      isAuthenticated = true;
+    }
+
+    if(status == 221){
+      console.log("Client close");
+    }else {
+      rl.question('Command : ', function (cmd) {
+        client.write(cmd)
+      })      
+    };
+
+  });
 });
--- server/src/server.js
+++ server/src/server.js
@@ -1,5 +1,9 @@
 import { createServer } from "net";
 
+const fs = require('fs');
+const users = JSON.parse(fs.readFileSync('./src/user.json'));
+const directory = process.cwd();
+
 export function launch(port) {
   const server = createServer((socket) => {
     console.log("new connection.");
@@ -10,8 +14,31 @@ export function launch(port) {
       console.log(command, args);
 
       switch(command) {
-        case "USER":
-          socket.write("230 User logged in, proceed.\r\n");
+        
+        case "USER":     
+          if (args[0] == undefined) {
+            socket.write('Error Please enter an username and a password.\r\n');
+            break;
+          }else{
+            socket.name = args[0];
+            let result = "";
+            users.forEach(user => {
+              if (user.name === socket.name){
+                socket.pass = user.pass;
+                result = '230 User logged in, proceed.\r\n';
+              }else{
+                result = '230 User not exist.\r\n';
+              }
+            });
+            socket.write(result);
+            break;  
+          }
+        case "PASS":
+          if (args[0] == undefined || args[0] != socket.pass) {
+            socket.write('Error Please enter a valid password.\r\n');
+          }else if (args[0] == socket.pass){
+            socket.write('331 Password valid.\r\n');
+          }
           break;
         case "SYST":
           socket.write("215 \r\n");
@@ -19,18 +46,41 @@ export function launch(port) {
         case "FEAT":
           socket.write("211 \r\n");
           break;
-        case "PWD":
-          socket.write("257 /users/dylan\r\n");
+        case "LIST":
+          socket.write(`\r\n`);
+          fs.readdirSync(process.cwd()).forEach(file => {
+            socket.write(`${file} \r\n`);
+          });
           break;
+        case "PWD":
+          socket.write(`257, ${process.cwd()} \r\n`);
+          break;
+        case "CWD":
+          if (args[0] == undefined) {
+            process.chdir(directory);
+            socket.write(`250 New path : ${process.cwd()} \r\n`);
+          }else {
+            try{
+              process.chdir(args[0]);
+              socket.write(`250 New path : ${process.cwd()} \r\n`);
+            } catch(err) {
+              socket.write(`Error Try another path.\r\n`);
+            }
+          }
+          break;
+        case "QUIT":
+          socket.write(`221 \r\n`);
+          socket.destroy();
         case "TYPE":
           socket.write("200 \r\n");
+          break;
+        case "TEST":
+          socket.write(`dirname : ${__dirname} cwd : ${process.cwd()} \r\n `);
           break;
         default:
           console.log("command not supported:", command, args);
       }
     });
-
-    socket.write("220 Hello World \r\n");
   });
 
   server.listen(port, () => {
